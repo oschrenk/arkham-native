15e4efe163812d0d63305544f3adcce39cb3c67a
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-08-10 18:16:45 +0200

    Start with Picker component

--- App.js
+++ App.js
@@ -14,9 +14,20 @@ export default class App extends React.Component {
           <Text>Open up App.js to start working on your app!</Text>
           <Text>Changes yofoooou make will automatically reload.</Text>
           <Text>Shake your phone to open the developer menu.</Text>
+          <NumberPicker/>
         </View>
       </Image>
     );
+  }
+}
+
+class NumberPicker extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Picker</Text>
+      </View>
+    )
   }
 }
 
