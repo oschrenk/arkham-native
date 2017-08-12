7fccdd708e66a82bda744a70dabbbf1c6dc88905
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-08-12 15:29:20 +0200

    Add [in|de]crement logic

--- App.js
+++ App.js
@@ -46,12 +46,28 @@ export default class App extends React.Component {
 
 class NumberPicker extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: parseInt(this.props.counter),
+    };
+  }
+
+  decrement(){
+    this.setState({ counter: this.state.counter - 1})
+  }
+
+  increment(){
+    this.setState({ counter: this.state.counter + 1})
+
+  }
+
   render() {
     return (
       <Image source={require('./img/damage-vertical.png')} style={counterStyles.container}>
-        <Text style={[counterStyles.half, counterStyles.left]} onPress={() => console.log('1st')}>-</Text>
-        <Text style={[counterStyles.half, counterStyles.right]} onPress={() => console.log('2nd')}>+</Text>
-        <Text pointerEvents="none" style={counterStyles.overlay} >{this.props.counter}</Text>
+        <Text style={[counterStyles.half, counterStyles.left]} onPress={this.decrement.bind(this)}>-</Text>
+        <Text style={[counterStyles.half, counterStyles.right]} onPress={this.increment.bind(this)}>+</Text>
+        <Text pointerEvents="none" style={counterStyles.overlay} >{this.state.counter}</Text>
       </Image>
     )
   }
